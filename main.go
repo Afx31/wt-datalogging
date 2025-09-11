ea7dba6485e5b07d37c2f54c5ad52109b94f05ec
Afx31 <post4wjm@gmail.com>
2025-09-11 20:32:47 +1000

    cleanup/syntax-formatting

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"log"
 	"math"
 	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 	"wt-datalogging/internal/tracks"
-	"path/filepath"
 
 	"github.com/stratoberry/go-gpsd"
 	"go.einride.tech/can/pkg/socketcan"
@@ -22,99 +22,100 @@ import (
 
 // --- MISC ---
 const SETTINGSFILEPATH = "wt-app-settings.json"
+
 // --- Data conversion constants ---
 // Oil Temp
 const OILTEMP_A = 0.0014222095
 const OILTEMP_B = 0.00023729017
-const OILTEMP_C = 9.3273998E-8
+const OILTEMP_C = 9.3273998e-8
 
 // Oil Pressure
-const OILPRESSURE_originalLow  float64 = 0    //0.5
-const OILPRESSURE_originalHigh float64 = 5    //4.5
-const OILPRESSURE_desiredLow   float64 = -100 //0
-const OILPRESSURE_desiredHigh  float64 = 1100 //1000
+const OILPRESSURE_originalLow float64 = 0    //0.5
+const OILPRESSURE_originalHigh float64 = 5   //4.5
+const OILPRESSURE_desiredLow float64 = -100  //0
+const OILPRESSURE_desiredHigh float64 = 1100 //1000
 
 type AppSettings struct {
-	LoggingHertz	int			`json:"loggingHertz"`
-	CanChannel		string	`json:"canChannel"`
-	Car						string	`json:"car"`
-	Ecu						string	`json:"ecu"`
-	Track					string	`json:"track"`
-  LapTiming			bool		`json:"lapTiming"`
+	LoggingHertz int    `json:"loggingHertz"`
+	CanChannel   string `json:"canChannel"`
+	Car          string `json:"car"`
+	Ecu          string `json:"ecu"`
+	Track        string `json:"track"`
+	LapTiming    bool   `json:"lapTiming"`
 }
 
 // --- Local variables to write to, which the datalogging will snapshot later ---
 type LapStats struct {
-  Type 						int8
-  LapCount 				uint8
-	BestLapTime 		uint32
-	PbLapTime 			uint32
+	Type            int8
+	LapCount        uint8
+	BestLapTime     uint32
+	PbLapTime       uint32
 	PreviousLapTime uint32
 }
 
 var (
-	appSettings *AppSettings
+	appSettings  *AppSettings
 	currentTrack tracks.Track
 	
-	localRpm						uint16
-	localSpeed					uint16
-	localGear						uint8
-	localVoltage				float64
-	localIat						uint16
-	localEct						uint16
-	localMil						uint8
-	localVts						uint8
-	localCl							uint8
-	localTps						uint16
-	localMap						uint16
-	localInj						uint16
-	localIgn						uint16
-	localLambdaRatio		float64
-	localKnockCounter		uint16
-	localTargetCamAngle	float64
-	localActualCamAngle	float64
-	localAnalog0				uint16 // Oil Temperature
-	localAnalog1				uint16 // Oil Pressure
-	localAnalog2				uint16
-	localAnalog3				uint16
-	localAnalog4				uint16
-	localAnalog5				uint16
-	localAnalog6				uint16
-	localAnalog7				uint16
-	localEthanolInput1	uint8
-	localEthanolInput2S300	float64
-	localEthanolInput2KPro 	uint8
-	localEthanolInput3 	uint16
+	localRpm               uint16
+	localSpeed             uint16
+	localGear              uint8
+	localVoltage           float64
+	localIat               uint16
+	localEct               uint16
+	localMil               uint8
+	localVts               uint8
+	localCl                uint8
+	localTps               uint16
+	localMap               uint16
+	localInj               uint16
+	localIgn               uint16
+	localLambdaRatio       float64
+	localKnockCounter      uint16
+	localTargetCamAngle    float64
+	localActualCamAngle    float64
+	localAnalog0           uint16 // Oil Temperature
+	localAnalog1           uint16 // Oil Pressure
+	localAnalog2           uint16
+	localAnalog3           uint16
+	localAnalog4           uint16
+	localAnalog5           uint16
+	localAnalog6           uint16
+	localAnalog7           uint16
+	localEthanolInput1     uint8
+	localEthanolInput2S300 float64
+	localEthanolInput2KPro uint8
+	localEthanolInput3     uint16
 
-	localLat							float64
-	localLon							float64
-	localTime							time.Time
-	localLapStartTime			time.Time
-	localCurrentLapTime		uint32
-	localLapCount					uint8
-	localBestLapTime			uint32
-	localPbLapTime				uint32
-	localPreviousLapTime	uint32
+	localLat             float64
+	localLon             float64
+	localTime            time.Time
+	localLapStartTime    time.Time
+	localCurrentLapTime  uint32
+	localLapCount        uint8
+	localBestLapTime     uint32
+	localPbLapTime       uint32
+	localPreviousLapTime uint32
 
 	lapStats = LapStats{Type: 3, LapCount: 1}
 )
 
 type CurrentLapData struct {
-	Type						int8
-  LapStartTime		time.Time
-	CurrentLapTime	uint32
-	PreviousLat			float64
-	PreviousLon			float64
+	Type           int8
+	LapStartTime   time.Time
+	CurrentLapTime uint32
+	PreviousLat    float64
+	PreviousLon    float64
 }
 
 func DataLoggingAtSpecificHertz(w *csv.Writer) {
-	startTimeStamp := []string{ time.Now().Format("02-01-2006 - 15:04:05"), appSettings.Track, appSettings.Car, appSettings.Ecu}
-	if  err := w.Write(startTimeStamp); err != nil {
+	startTimeStamp := []string{time.Now().Format("02-01-2006 - 15:04:05"), appSettings.Track, appSettings.Car, appSettings.Ecu}
+	if err := w.Write(startTimeStamp); err != nil {
 		log.Fatalln("Error writing datalogging start timestamp CSV")
 	}
 
-	csvHeaders := []string{"HertzTime","Engine RPM","Speed","Gear","Voltage","IAT","ECT","MIL","VTS","CL","TPS","MAP","INJ","IGN","Lambda Ratio","Knock Count","Target Cam Angle","Actual Cam Angle","Analog0","Analog1","Analog2","Analog3","Analog4","Analog5","Analog6","Analog7","Ethanol Input1","Ethanol Input2","Ethanol Input3","Latitude","Longitude","LapCount","CurrentTime","CurrentLapStartTime","CurrentLapTime","BestLapTime","PbLapTime","PreviousLapTime"}
-	csvHeaderTypes := []string{"sec","rpm","km/h","","V","C","C","","","","%","kPa","ms","deg","lambda","count","deg","deg","","","","","","","","","hz","%","%","deg","deg","","","sec","sec","sec","sec","sec","sec"}
+	csvHeaders := []string{"HertzTime", "Engine RPM", "Speed", "Gear", "Voltage", "IAT", "ECT", "MIL", "VTS", "CL", "TPS", "MAP", "INJ", "IGN", "Lambda Ratio", "Knock Count", "Target Cam Angle", "Actual Cam Angle", "Analog0", "Analog1", "Analog2", "Analog3", "Analog4", "Analog5", "Analog6", "Analog7", "Ethanol Input1", "Ethanol Input2", "Ethanol Input3", "Latitude", "Longitude", "LapCount", "CurrentTime", "CurrentLapStartTime", "CurrentLapTime", "BestLapTime", "PbLapTime", "PreviousLapTime"}
+	csvHeaderTypes := []string{"sec", "rpm", "km/h", "", "V", "C", "C", "", "", "", "%", "kPa", "ms", "deg", "lambda", "count", "deg", "deg", "", "", "", "", "", "", "", "", "hz", "%", "%", "deg", "deg", "", "", "sec", "sec", "sec", "sec", "sec", "sec"}
 	if err := w.Write(csvHeaders); err != nil {
 		log.Fatalln("Error writing headers to CSV")
 	}
@@ -145,13 +146,13 @@ func DataLoggingAtSpecificHertz(w *csv.Writer) {
 			formattedLapStartTime := localLapStartTime.Format("15:04:05 02-01-2006")
 			
 			var ethanolInput2 string
-			if (appSettings.Ecu == "s300") {
+			if appSettings.Ecu == "s300" {
 				ethanolInput2 = strconv.FormatFloat(float64(localEthanolInput2S300), 'f', 2, 64)
 			} else {
 				ethanolInput2 = strconv.FormatUint(uint64(localEthanolInput2KPro), 10)
 			}
 			
-			csvFrame := []string {
+			csvFrame := []string{
 				time,
 				strconv.FormatUint(uint64(localRpm), 10),
 				strconv.FormatUint(uint64(localSpeed), 10),
@@ -212,7 +213,7 @@ func isThisTheFinishLine(x3 float64, y3 float64, x4 float64, y4 float64) bool {
 	denominator := (x3 - x4) * (y1 - y2) - (y3 - y4) * (x1 - x2)
 
 	// If denominator is 0, the lines are parallel or coincident
-	if (math.Abs(denominator) < 1e-10) {
+	if math.Abs(denominator) < 1e-10 {
 		return false
 	}
 
@@ -279,7 +280,7 @@ func handleGpsDatalogging() {
       
       // Start the next lap
       currentLapData.LapStartTime = convertedCurrentTime
-      lapStats.LapCount++;
+			lapStats.LapCount++
 
 	    // --- Update local values for the datalog ---
 			localLapCount = lapStats.LapCount
@@ -345,9 +346,9 @@ func main() {
 	root := os.DirFS(dataPath)
 	// Find CSV files
 	mdFiles, err := fs.Glob(root, "*.csv")
-	counter := 1;
+	counter := 1
 	for range mdFiles {
-		counter++;
+		counter++
 	}
 
 	// Now do the file creation
@@ -371,7 +372,7 @@ func main() {
 	go DataLoggingAtSpecificHertz(writer)
 
 	// Do GPS datalogging
-	if (appSettings.LapTiming) {
+	if appSettings.LapTiming {
 		go handleGpsDatalogging()
 	}
 
@@ -394,7 +395,7 @@ func main() {
 		case 661, 1633:
 			localIat = binary.BigEndian.Uint16(frame.Data[0:2])
 			localEct = binary.BigEndian.Uint16(frame.Data[2:4])
-			if (appSettings.Ecu == "kpro") {
+			if appSettings.Ecu == "kpro" {
 				localMil = frame.Data[4]
 				localVts = frame.Data[5]
 				localCl = frame.Data[6]
@@ -414,13 +415,13 @@ func main() {
 		
 		// K-Pro only
     case 665, 1637:
-      if (appSettings.Ecu == "kpro") {
+			if appSettings.Ecu == "kpro" {
         localKnockCounter = binary.BigEndian.Uint16(frame.Data[0:2])
       }
 
     // K-Pro only
     case 666, 1638:
-      if (appSettings.Ecu == "kpro") {
+			if appSettings.Ecu == "kpro" {
         localTargetCamAngle = float64(binary.BigEndian.Uint16(frame.Data[0:2]))
         localActualCamAngle = float64(binary.BigEndian.Uint16(frame.Data[2:4]))
       }
@@ -447,11 +448,11 @@ func main() {
 		case 669, 1641:
       localEthanolInput1 = frame.Data[0]
 
-			if (appSettings.Ecu == "s300") {
+			if appSettings.Ecu == "s300" {
       	localEthanolInput2S300 = float64(frame.Data[1]) * 2.56 // Duty
-      	localEthanolInput3 = uint16(frame.Data[2]) // Ethanol Content
-			} else if (appSettings.Ecu == "kpro") {
-      	localEthanolInput2KPro = frame.Data[1] // Ethanol Content
+				localEthanolInput3 = uint16(frame.Data[2])             // Ethanol Content
+			} else if appSettings.Ecu == "kpro" {
+				localEthanolInput2KPro = frame.Data[1]                        // Ethanol Content
 				localEthanolInput3 = binary.BigEndian.Uint16(frame.Data[2:4]) // Fuel Temperature
 			}
 		}
